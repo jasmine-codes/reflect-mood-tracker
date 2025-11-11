f281236c27a348de3137f950c01bce205fdee473
jasmine-codes <jsmlee97@gmail.com>
2025-11-11 17:09:12 +0100

    create class JournalEntry

--- Program.cs
+++ Program.cs
@@ -3,3 +3,13 @@ using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
 using System.Linq;
+
+public class JournalEntry
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime Date { get; set; } = DateTime.Today;
+    public int Mood { get; set; }
+    public string Title { get; set; }
+    public string Notes { get; set; }
+    public List<string> Tags { get; set; } = new List<string>();
+}
