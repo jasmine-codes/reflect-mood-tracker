db43f562566bfe2d82b2f156df1f87f106b15c23
jasmine-codes <jsmlee97@gmail.com>
2025-11-11 17:03:42 +0100

    clean boilerplate and setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
