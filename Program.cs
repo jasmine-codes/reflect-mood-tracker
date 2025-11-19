e96b71f47d341722379a237e008389b21593c3fa
jasmine-codes <jsmlee97@gmail.com>
2025-11-19 10:02:58 +0100

    view all entries

--- Program.cs
+++ Program.cs
@@ -183,7 +183,7 @@ class Program
         }
 
         var sorted = entries.OrderByDescending(x => x.Date).ThenByDescending(x => x.Id).ToList();
-
+        PrintEntriesList(sorted); //helper method -> to be implemented
     }
 
     #endregion
